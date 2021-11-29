58e576c2122bd0b6ad4167ed3c1506a9bd4fd0c1
Itz-kavish <91275857+Itz-kavish@users.noreply.github.com>
2021-11-29 14:34:24 -0800

    Add files via upload

---
+++ Javascri-t-projects/Color flipper/hex.js
@@ -0,0 +1,18 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const button = document.getElementById("btn");
+const color = document.getElementsByClassName("color")[0];
+
+function generateRandomNum(){
+    return Math.floor(Math.random()*hex.length);
+}
+
+document.addEventListener('click', function(){
+    let hexColor = '#';
+    for(i=0;i<6;i++){
+        hexColor += hex[generateRandomNum()];
+    }
+    console.log(hexColor);
+    document.body.style.backgroundColor = hexColor;
+    color.innerHTML = hexColor;
+})
