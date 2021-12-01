bce2a0868498f1a9f496fd2e6fccca7fa19cd98f
Itz-kavish <kavisharora159@gmail.com>
2021-12-01 11:06:47 -0800

    main

--- Javascri-t-projects/Reviews/app.js
+++ Javascri-t-projects/Reviews/app.js
@@ -4,7 +4,7 @@ const reviews = [
     id: 1,
     name: "Virat Chokli",
     job: "Professional cricketer",
-    img: "viratKohli",
+    img: "viratKohli.jpg",
     text: "Virat Chokli is a professional cricketer. His name used to be Virat kohli but due to his multiple instances of choking during important matches and winning all the not very important matches, he is also reffered as virat CHOKLI. His team never wins trophies but are great at winning a people's hearts. There trophy cabinet was renamed to the HEART cabinet because of the excessive amount of hearts they won.",
   },
   {
@@ -12,14 +12,14 @@ const reviews = [
     name: "anna johnson",
     job: "web designer",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    text: "The company I work at is a company that i work at. I usually get paid. My monthly salary ranges between 5-10 cheeseburgers. All this burgers are worth a lot. Once there were 3 bananas on an apple tree that i liked very much but the apples were not that tasty.",
   },
   {
     id: 3,
     name: "peter jones",
     job: "intern",
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    text: "Sriracha sauce is a sweet sauce that is very spicy. It is so spicy that I can raise the sugar levels in your blood exponentially. I used to eat few apples a day but I realized that I need to but a new car so I started eating other fruits like lettuce, peas and caramel onions. Caramel onion are caramel apples but onions.",
   },
   {
     id: 4,
